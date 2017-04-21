24f48b8334b06de557fe994d69e6ac7fc05977fc
gopiraj24 <gopiraj.dorairaj@gmail.com>
2017-04-21 09:08:22 -0400

    Initil commit

---
+++ lib/ident.ex
@@ -0,0 +1,18 @@
+defmodule Ident do
+  @moduledoc """
+  Documentation for Ident.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ident.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ident_test.exs
@@ -0,0 +1,8 @@
+defmodule IdentTest do
+  use ExUnit.Case
+  doctest Ident
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
